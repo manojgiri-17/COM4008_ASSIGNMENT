9e28438fba38b545783a2b26e66a1697965c5e55
ashishbk1415-creator <ashishbk1415@gmail.com>
2025-11-25 15:25:51 +0000

    setting

--- assignment-space_invaders.py
+++ assignment-space_invaders.py
@@ -2,3 +2,13 @@ import pygame
 import random
 import sys
 import os
+
+# ==========================================
+# SETTINGS
+# ==========================================
+pygame.init()
+WIDTH, HEIGHT = 600, 700
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Invaders")
+clock = pygame.time.Clock()
+FPS = 60
