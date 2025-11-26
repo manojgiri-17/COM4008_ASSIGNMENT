d1b13b2061c1446cf81e0b06c85dceef726b0cd2
ashishbk1415-creator <ashishbk1415@gmail.com>
2025-11-26 10:42:15 +0000

    load asset

--- assignment-space_invaders.py
+++ assignment-space_invaders.py
@@ -68,3 +68,23 @@ def save_highscore(value):
             f.write(str(int(value)))
     except:
         pass
+
+# ==========================================
+# LOAD ASSETS
+# ==========================================
+shoot_sound = load_sound(SOUND_SHOOT)
+explode_sound = load_sound(SOUND_EXPLODE)
+music_loaded = load_music(MUSIC_BG)
+heart_img = load_image(HEART_IMG)
+background_img = load_image(BACKGROUND_IMG)
+game_over_img = load_image(GAME_OVER_IMG)
+
+explosion_frames = []
+for i in range(EXPLOSION_FRAME_COUNT):
+    f = load_image(f"explosion_{i}.png")
+    if f:
+        explosion_frames.append(f)
+
+inv1_img = load_image("invader1.png")
+inv2_img = load_image("invader2.png")
+inv3_img = load_image("invader3.png")
